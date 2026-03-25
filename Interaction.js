92a0fcab22bd3c55c849f1b70ce7a3b0b7363ac9
sohil-khann <khaa40408@gmail.com>
2026-03-25 12:23:17 +0530

    [Sohil khan] Add. Javascript

---
+++ Interaction.js
@@ -0,0 +1,16 @@
+// alert(hello);
+
+result = prompt(title, ["Option 1", "Option 2", "Option 3"]);
+
+console.log(result);
+
+// let age = prompt('How old are you?', 100);
+
+// alert(`You are ${age} years old!`); // You are 100 years old!
+
+// result = confirm(question);
+
+// let isBoss = confirm("Are you the boss?");
+
+// alert( isBoss ); // true if OK is pressed
+
